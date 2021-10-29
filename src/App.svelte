ea669c61467ce5e9536e03b36d996ab5a9aa40d4
Zachiah <zachiahsawyer1@gmail.com>
2021-10-28 20:42:51 -0700

    v1

---
+++ src/App.svelte
@@ -0,0 +1,135 @@
+<script lang="ts">
+import TextField from "./lib/TextField.svelte";
+
+  const LETTERS = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+
+  const prime: Filter = (n) => {
+    if (n < 2) {
+      return false;
+    }
+    for (let i = 2; i < Math.sqrt(n); i++) {
+      if (n % i === 0) {
+        return false;
+      }
+    }
+    return true;
+  };
+
+  const perfectSquare: Filter = (n) => {
+    const sqrt = Math.sqrt(n);
+    return sqrt % 1 === 0;
+  };
+
+  const all: Filter = (n) => true;
+
+  const add: (n: number | string) => Step = (a) => (c, decode) => {
+    if (typeof a === "string") {
+      a = LETTERS.indexOf(a.toLowerCase());
+    }
+
+    if (decode) {
+      return LETTERS[(26 + LETTERS.indexOf(c.toLowerCase()) - a) % 26];
+    } else {
+      return LETTERS[(26 + a + LETTERS.indexOf(c.toLowerCase())) % 26];
+    }
+  };
+
+  // Here is the api I want
+  // STEPS = [
+  //     action(prime)(add(1)),
+  //     action(all)(pass('abc'))
+  // ]
+
+  type Filter = (n: number) => boolean;
+  type Step = (c: string, decode: boolean) => string;
+
+  const action =
+    (filter: Filter) => (step: Step) => (text: string) => (decode: boolean) => {
+      return text
+        .split("")
+        .map((c, i) => {
+          if (filter(i)) {
+            return step(c, decode);
+          }
+          return c;
+        })
+        .join("");
+    };
+
+  const STEPS: ((text: string) => (decode: boolean) => string)[] = [
+    action(all)(add(1)),
+    action(prime)(add(1)),
+  ];
+
+  const decode = (text: string) => {
+    return STEPS.reverse().reduce((t: string, step) => {
+      return step(t)(true);
+    }, text);
+  };
+
+  const encode = (text: string) => {
+    return STEPS.reduce((t: string, step) => {
+      return step(t)(false);
+    }, text);
+  };
+
+  //@ts-ignore
+  (window.encode = encode) && (window.decode = decode);
+
+  console.log();
+
+  let text = "";
+
+  $: encodedText = encode(text);
+</script>
+
+<div class="h-screen flex items-center p-2">
+<div class="flex flex-col w-100 max-w-full m-auto gap-4">
+
+    <h1 class="text-center text-xl font-bold bg-blue-200 rounded-md p-2">Coder!</h1>
+<TextField
+  label="Text"
+  value={text}
+    on:input={(e) => {
+        //@ts-ignore
+        text = e.target.value;
+    }}
+/>
+
+<TextField
+  label="Encoded Text"
+  value={encodedText}
+  on:input={(e) => {
+      //@ts-ignore
+      text = decode(e.target.value);
+  }}
+/>
+</div>
+</div>
---
+++ src/lib/TextField.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  export let value: string;
+  export let label: string;
+</script>
+
+<label class="p-2 bg-blue-200 block flex flex-col rounded-md">
+  <p class="mb-2">{label}</p>
+
+  <input
+    {value}
+    on:input
+    class="p-2 bg-gray-200 rounded-md duration-200 outline-none focus:(bg-gray-300)"
+  />
+</label>
